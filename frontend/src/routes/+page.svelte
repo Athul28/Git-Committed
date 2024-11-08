e7f842af99ac218bba0ba57a749350d587446667
NagavarapuSaarvari <149094209+NagavarapuSaarvari@users.noreply.github.com>
2024-11-08 19:51:23 +0530

    Updated Nav Bar

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,24 +4,40 @@
     import Navbar from "../lib/components/Navbar/+page.svelte"
 </script>
 
+<style>
+	.details{
+		text-indent: 5px;
+	}
+	.signedInText{
+  	font-size: 20px;
+	}
+	.notSignedInText{
+  	font-size: 20px;
+	}
+	.buttonPrimary{
+		text-align: center;
+	}
+</style>
+
 <Navbar />
-<h1>SvelteKit Auth Example</h1>
-<div>
-	{#if $page.data.session}
-		{#if $page.data.session.user?.image}
-			<img src={$page.data.session.user.image} class="avatar" alt="User Avatar" />
+<nav class="border">
+	<div class="details">
+		{#if $page.data.session}
+			{#if $page.data.session.user?.image}
+				<img src={$page.data.session.user.image} class="avatar" alt="User Avatar"/>
+			{/if}
+			<p class="signedInText">
+				<small>Signed in as</small><br />
+				<strong>{$page.data.session.user?.name ?? 'User'}</strong>
+			</p>
+			<SignOut>
+				<div slot="submitButton" class="buttonPrimary">Sign out</div>
+			</SignOut>
+		{:else}
+			<span class="notSignedInText">You are not signed in</span>
+			<SignIn>
+				<div slot="submitButton" class="buttonPrimary">Sign in</div>
+			</SignIn>
 		{/if}
-		<span class="signedInText">
-			<small>Signed in as</small><br />
-			<strong>{$page.data.session.user?.name ?? 'User'}</strong>
-		</span>
-		<SignOut>
-			<div slot="submitButton" class="buttonPrimary">Sign out</div>
-		</SignOut>
-	{:else}
-		<span class="notSignedInText">You are not signed in</span>
-		<SignIn>
-			<div slot="submitButton" class="buttonPrimary">Sign in</div>
-		</SignIn>
-	{/if}
-</div>
+	</div>
+</nav>
