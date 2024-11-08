80b2207471304661c099fe4624bcc03546951cd2
Sammonster495 <samarthshetty495@gmail.com>
2024-11-08 18:29:42 +0530

    feat: database definition

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,4 +1,4 @@
-import { DefaultSession, SvelteKitAuth, SvelteKitAuthConfig } from '@auth/sveltekit';
+import { SvelteKitAuth, SvelteKitAuthConfig } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
 import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET } from '$env/static/private';
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -12,23 +12,6 @@ const authOptions: SvelteKitAuthConfig = {
             clientSecret: AUTH_GOOGLE_SECRET,
         }),
     ],
-    callbacks: {
-        async signIn({ user, account, profile }) {
-            const existingUser = await prisma.user.findUnique({
-                where: { email: user.email },
-            });
-
-            if (existingUser && existingUser.id !== user.id) {
-                // Link the accounts
-                await prisma.account.update({
-                    where: { userId: existingUser.id },
-                    data: { userId: user.id },
-                });
-            }
-
-            return true;
-        },
-    },
     trustHost: true, // Move this line outside of the callbacks object
 };
 
