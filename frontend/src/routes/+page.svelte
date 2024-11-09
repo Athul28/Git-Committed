aba228afad57171f6aea99933b541a464d266c10
Athul Bhandary <athulbhandary28@gmail.com>
2024-11-09 08:46:59 +0530

    fix bugs and improved styles

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -10,9 +10,13 @@
 		<p class="text-xl  text-gray-800">
 			Healthcare systems often face critical shortages of essential medical supplies, such as
 			personal protective equipment (PPE), medications, and medical devices, due to sudden surges in
-			demand, supply chain disruptions, or limited availability in certain regions
+			demand, supply chain disruptions, or limited availability in certain regions.
+		</p>
+		<p class="text-xl font-bold">
+			But we are here to solve it!!!
 		</p>
 		<Button class="bg-blue-500 text-xl mt-10 px-6 py-6 hover:scale-110 transition" on:click={() => { goto('/find'); }}>Explore Solutions</Button>
+		<Button class="bg-white text-blue-500 border-2 border-blue-500 text-xl mt-10 px-6 py-6 hover:scale-110 transition ml-5 hover:text-white" on:click={() => { goto('/inventory'); }}>Inventory</Button>
 	</div>
 	<div>
 		<img src={home} alt="home-image" />
